9de7b02f7159b23d8aa3f1d3b39d1ae04c5d5bba
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-04-08 14:54:08 +0100

    removed commented code && changed a test method name

--- tests/Services/Reader/ConvertFileTest.php
+++ tests/Services/Reader/ConvertFileTest.php
@@ -10,15 +10,8 @@ class ConvertFileTest extends TestCase
     protected $new_file = "C:/Users/jvniekerk/Desktop/repos/file-upload-convert/resources/sample-files/customers.json";
     protected $filename = "http://localhost/file-upload-convert/resources/sample-files/customers.csv";
 
-    public function test()
+    public function testConvertFile()
     {
-
-
-//            'FileLocation'  => "http://localhost/file-upload-convert/resources/sample-files/customers.csv",
-//            'NewFileType'   => "json",
-//            'SaveOrDisplay' => true,
-//            'SaveLocation'  => $this->new_file,
-
         $this->assertClassHasAttribute('filename', ConvertFile::class);
         $converter = new ConvertFile($this->filename, 'yaml', new FileHelpers());
         $this->assertIsObject($converter);
